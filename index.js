57ffbe1c1e18896628c6bc477c5a5b861500dc37
Sébastien Gaya <sebastien.gaya@gmail.com>
2024-06-20 13:06:48 +0200

    listening

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-
+const { networkInterfaces, hostname } = require('os');
+const port = 3000;
 const express = require('express')
 const app = express()
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+
+const nets = networkInterfaces();
+const hostnames = ["localhost", "*"];
+for (const name of Object.keys(nets)) {
+  for (const net of nets[name]) {
+    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    // 'IPv4' is in Node <= 17, from 18 it's a number 4 or 6
+    const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+    if (net.family === familyV4Value && !net.internal) {
+      hostnames.push(net.address);
+    }
+  }
+}
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
@@ -24,9 +38,11 @@ io.on('connection', (socket) => {
     console.log(touch.ratio.x)
     io.emit('reception', touch);
   });
-
 });
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log(`listening on:`);
+  hostnames.forEach(hostname => {
+    console.log(`- http://${hostname}:${port}`);
+  })
 });
